3658b91c3b5e9df3370baa8b51f3e2690546e6f7
luthfeew <luthfeew@gmail.com>
2022-12-21 02:30:31 +0700

    update

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 import genshin
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 cookies = {"ltuid": 119480035, "ltoken": "cnF7TiZqHAAvYqgCBoSPx5EjwezOh1ZHoqSHf7dT"}
 client = genshin.Client(cookies)
+
+uncensor_dict = {
+    "A": "А",
+    "a": "а",
+    "E": "Е",
+    "e": "е",
+    "O": "О",
+    "o": "о",
+    "I": "І",
+    "i": "і",
+}
+uncensor_dict_ext = {
+    "B": "В",
+    "K": "К",
+    "M": "М",
+    "H": "Н",
+    "P": "Р",
+    "p": "р",
+    "C": "С",
+    "c": "с",
+    "T": "Т",
+    "y": "у",
+    "X": "Х",
+    "x": "х",
+    "V": "Ѵ",
+    "v": "ѵ",
+    "u": "υ",
+}
 
 
 @app.get("/")
@@ -12,20 +40,38 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/uncensor/{text}")
+def uncensor(text: str, ext: bool = False):
+    if ext:
+        uncensor_dict.update(uncensor_dict_ext)
+    for key, value in uncensor_dict.items():
+        text = text.replace(key, value)
+    return {"text": text}
+
+
 @app.get("/full/{uid}")
 async def read_item(uid: int):
-    data = await client.get_full_genshin_user(uid)
+    try:
+        data = await client.get_full_genshin_user(uid)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     return {"uid": uid, "data": data}
 
 
 @app.get("/partial/{uid}")
 async def read_item(uid: int):
-    data = await client.get_partial_genshin_user(uid)
+    try:
+        data = await client.get_partial_genshin_user(uid)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     return {"uid": uid, "data": data}
 
 
 @app.get("/abyss/{uid}")
 async def read_item(uid: int):
-    data = await client.get_full_genshin_user(uid)
+    try:
+        data = await client.get_full_genshin_user(uid)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     abyss = data.abyss.current if data.abyss.current.floors else data.abyss.previous
     return {"uid": uid, "data": abyss}
