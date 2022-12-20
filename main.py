984025c90df475d8960cbe57c87a4ed22819b439
luthfeew <luthfeew@gmail.com>
2022-12-21 01:57:09 +0700

    genshin.py

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
-def app(event):
-    return "Hello, world!"
+import genshin
+from fastapi import FastAPI
+
+app = FastAPI()
+
+cookies = {"ltuid": 119480035, "ltoken": "cnF7TiZqHAAvYqgCBoSPx5EjwezOh1ZHoqSHf7dT"}
+client = genshin.Client(cookies)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/full/{uid}")
+async def read_item(uid: int):
+    data = await client.get_full_genshin_user(uid)
+    return {"uid": uid, "data": data}
+
+
+@app.get("/partial/{uid}")
+async def read_item(uid: int):
+    data = await client.get_partial_genshin_user(uid)
+    return {"uid": uid, "data": data}
+
+
+@app.get("/abyss/{uid}")
+async def read_item(uid: int):
+    data = await client.get_full_genshin_user(uid)
+    abyss = data.abyss.current if data.abyss.current.floors else data.abyss.previous
+    return {"uid": uid, "data": abyss}
