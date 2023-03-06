593105e8db386e1e33b2a8b7dd1fd0b5d94addd1
luthfeew <luthfeew@gmail.com>
2023-03-06 22:30:43 +0700

    revert

--- main.py
+++ main.py
@@ -4,18 +4,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://127.0.0.1:5173/",
-    "https://tahubulat.tech/",
-    "https://www.tahubulat.tech/",
-    "https://tahubulat-tech.vercel.app/",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
+    allow_origins=["*"],
     allow_headers=["*"],
 )
 
